78eac199aac54c3b14298dea5cee5b26d1031071
huangzhyf <huangzhyf@yonyou.com>
2018-08-03 20:08:08 +0800

    app.js

--- app.js
+++ app.js
@@ -1,4 +1,34 @@
 /**
  * Created by Administrator on 2018/8/3.
  */
-const Koa=require('koa');
+var fs = require('fs');
+var path = require('path');
+var koa = require('koa');
+var router = require('koa-router')();
+var serve = require('koa-static');
+var koaBody = require('koa-body');
+var gzip = require('koa-gzip');
+var logger = require('koa-logger');
+
+var app = koa();
+app.use(koaBody({
+  formidable: {uploadDir: __dirname},
+  textLimit: '50mb',
+  formLimit: '50mb'
+}));
+app.use(router.routes())
+  .use(router.allowedMethods());
+
+// response
+app.use(function *(next){
+  // (3) 进入 response 中间件，没有捕获到下一个符合条件的中间件，传递到 upstream
+  if (this.request.url === '/') {
+    this.body = fs.readFileSync(path.resolve(__dirname, '../dist/index.html')).toString();
+  } else {
+    return yield* next;
+  }
+});
+
+app.use(logger());
+app.use(serve(path.join(__dirname, '../')));
+app.listen( 8089 );
--- src/config/config.js
+++ src/config/config.js
@@ -2,8 +2,8 @@ module.exports = {
   version: '1.0',
   development: {
     host: window.location.protocol + '//' + window.location.host, //当前域名
-    // apiHost: 'http://13ede50d.ngrok.io', //后端接口域名
-    apiHost: 'http://10.4.111.31:9090', //后端接口域名
+    apiHost: 'http://13ede50d.ngrok.io', //后端接口域名
+    // apiHost: 'http://10.4.111.31:9090', //后端接口域名
   },
   test: {
     host: window.location.protocol + '//' + window.location.host, //当前域名
@@ -11,10 +11,10 @@ module.exports = {
   },
   prevProduction: {
     host: window.location.protocol + '//' + window.location.host, //当前域名
-    apiHost: 'https://ys.esn.ren', //后端接口域名
+    apiHost: 'http://47.94.115.87/globalmate/', //后端接口域名
   },
-  distProduction: {
+  production: {
     host: window.location.protocol + '//' + window.location.host, //当前域名
-    apiHost: 'https://ys.yyuap.com', //后端接口域名
+    apiHost: 'http://47.94.115.87/globalmate/', //后端接口域名
   }
 }
