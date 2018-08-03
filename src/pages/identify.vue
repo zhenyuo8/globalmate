4efc90b569d86a58a00340a76e88e97900bf4c41
huangzhyf <huangzhyf@yonyou.com>
2018-08-03 19:37:47 +0800

    identify page

--- src/pages/identify.vue
+++ src/pages/identify.vue
@@ -1,13 +1,35 @@
 <style scoped>
-
+  #identify{
+      font-size: 14px;
+      width: 6.7rem;
+      height: 13.34rem;
+      background: #eee;
+      padding: 0.4rem;
+  }
+  .identify_body{
+      width: 70%;
+      margin: auto;
+  }
+  .identify_body>div{
+      height: 140px;
+      line-height: 140px;
+      background: #fff;
+      margin-top: 20px;
+      border-radius: 4px;
+  }
 
 </style>
 
 <template>
 
-<div class="identify">
+<div class="identify" id="identify">
     <div class="identify_body">
-
+        <div class="identify_face_page">
+          身份证正面
+        </div>
+        <div class="identify_opposite_page">
+          身份证反面
+        </div>
     </div>
 </div>
 
