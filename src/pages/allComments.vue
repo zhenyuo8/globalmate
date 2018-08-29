b47dbefe69cd73d62e82a5fb3e07011ff5335fb6
huangzhyf <huangzhyf@yonyou.com>
2018-08-29 21:27:48 +0800

    allComment conflict

--- src/pages/allComments.vue
+++ src/pages/allComments.vue
@@ -1,25 +1,25 @@
 <template>
-  <div class="">
-    <div class="mineInformation_comment_warp">
-      <div class="comment_repeat" v-for="(item,index) in commentList" :key='index'>
-        <p class="comment_repeat_top">
-          <img :src="item.pic" alt="">
-          <span>{{item.evaluation.uEvluatorName}}</span>
-          <span class="score">评分:{{item.evaluation.score}}</span>
-        </p>
-        <p class="comment_repeat_middle">
-          {{item.evaluation.createTime}} 标题:{{item.needAggEntity.conceretNeed.title}} 悬赏金额:{{item.needAggEntity.conceretNeed.rewardAmount}}
-        </p>
-        <p class="comment_repeat_bottom">
-          {{item.evaluation.content}}
-        </p>
-      </div>
+    <div class="">
+        <div class="mineInformation_comment_warp">
+            <div class="comment_repeat" v-for="(item,index) in commentList" >
+                <p class="comment_repeat_top">
+                    <img :src="item.pic" alt="">
+                    <span>{{item.evaluation.uEvluatorName}}</span>
+                    <span class="score">评分:{{item.evaluation.score}}</span>
+                </p>
+                <p class="comment_repeat_middle">
+                    {{item.evaluation.createTime}} 标题:{{item.needAggEntity.conceretNeed.title}} 悬赏金额:{{item.needAggEntity.conceretNeed.rewardAmount}}
+                </p>
+                <p class="comment_repeat_bottom">
+                    {{item.evaluation.content}}
+                </p>
+            </div>
 
+        </div>
+         <div class="defindloadig" v-if="loadingShow">
+             <loading></loading>
+         </div>
     </div>
-    <div class="defindloadig" v-if="loadingShow">
-      <loading></loading>
-    </div>
-  </div>
 </template>
 
 <script>
@@ -111,87 +111,70 @@
                 }
               }, 200);
             }
-          }
-        }
-        this.loadingShow = false;
-      }
-    }
-  },
-  activated() {
-    this.commentList = [];
-    this.isOthers = this.$route.query.isOthers;
-    if (this.userInfo.token) {
-      this.getEvalute();
-    } else {
-      this.time = setInterval(() => {
-        if (this.userInfo.token) {
-          this.getEvalute();
+		},
+        deactivated() {
           clearInterval(this.timer);
-        }
-      }, 200);
-    }
-  },
-  deactivated() {
-    clearInterval(this.timer);
-  },
-  created() {}
-};
+        },
+		created(){
+
+		}
+	}
 </script>
 
 <style>
-.mineInformation_comment_warp {
-  position: relative;
-  font-size: 14px;
-  background: #f7f5f3;
-  padding: 10px 0;
-}
-.mineInformation_comment_warp > div {
-  background: #fff;
-  margin-bottom: 10px;
-  padding: 10px 0.4rem;
-  font-size: 14px;
-}
-.mineInformation_comment_warp .mineInformation_comment_header {
-  margin-bottom: 0;
-  text-align: left;
-}
-.mineInformation_comment_warp > div > .comment_repeat_top img {
-  width: 0.8rem;
-  height: 0.8rem;
-  border-radius: 4px;
-}
-.comment_repeat_top {
-  display: flex;
-  flex-direction: row;
-  position: relative;
-}
-.comment_repeat_top span {
-  line-height: 0.8rem;
-  margin-left: 0.2rem;
-  color: #333;
-}
-.comment_repeat_top .score {
-  position: absolute;
-  right: 0;
-  color: #ff0023;
-}
-.comment_repeat_middle {
-  margin-top: 10px;
-  font-size: 12px;
-  color: #999;
-  text-align: left;
-}
-.comment_repeat_bottom {
-  margin-top: 10px;
-  text-align: left;
-}
-.mineInformation_comment_warp > p {
-  width: 2rem;
-  margin: auto;
-  color: #e42641;
-  border: 1px solid #e42641;
-  background: #fff;
-  padding: 6px 0.3rem;
-  border-radius: 16px;
-}
+    .mineInformation_comment_warp{
+        position: relative;
+        font-size: 14px;
+        background: #f7f5f3;
+        padding: 10px 0;
+    }
+    .mineInformation_comment_warp>div{
+        background: #fff;
+        margin-bottom: 10px;
+        padding: 10px 0.4rem;
+        font-size: 14px;
+    }
+    .mineInformation_comment_warp .mineInformation_comment_header{
+        margin-bottom: 0;
+        text-align: left;
+    }
+    .mineInformation_comment_warp>div>.comment_repeat_top img{
+        width: .8rem;
+        height: 0.8rem;
+        border-radius:4px;
+    }
+    .comment_repeat_top{
+        display: flex;
+        flex-direction: row;
+        position: relative;
+    }
+    .comment_repeat_top span{
+        line-height: 0.8rem;
+        margin-left: .2rem;
+        color: #333;
+    }
+    .comment_repeat_top .score{
+        position: absolute;
+        right: 0;
+        color: #ff0023;
+    }
+    .comment_repeat_middle{
+        margin-top: 10px;
+        font-size: 12px;
+        color: #999;
+        text-align: left;
+    }
+    .comment_repeat_bottom{
+        margin-top: 10px;
+        text-align: left;
+    }
+    .mineInformation_comment_warp>p{
+        width: 2rem;
+        margin: auto;
+        color: #e42641;
+        border: 1px solid #e42641;
+        background: #fff;
+        padding: 6px 0.3rem;
+        border-radius: 16px;
+    }
 </style>
