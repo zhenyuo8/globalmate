589960ced801b981d0191200e62dffcbadb3905a
huangzhyf <huangzhyf@yonyou.com>
2018-09-09 15:21:18 +0800

    delete plupload

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -68,6 +68,7 @@ Vue.component(MessageBox.name, MessageBox);
 Vue.component(Swipe.name, Swipe);
 Vue.component(SwipeItem.name, SwipeItem);
 import userMix from "../mixins/userInfo";
+let url=require('../assets/images/index_swiper1.jpeg')
 export default {
   name: "index",
   mixins: [userMix],
@@ -77,9 +78,9 @@ export default {
   data() {
     return {
       slides: [
-        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511015180050&di=0d2ee92eead284e8133d6df07535d75a&imgtype=0&src=http%3A%2F%2Fimg.sc115.com%2Fuploads1%2Fsc%2Fjpgs%2F1512%2Fapic16988_sc115.com.jpg",
-        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511015180167&di=7412fd486c47c15f1d27485be0d7bd28&imgtype=0&src=http%3A%2F%2Fwww.duoxinqi.com%2Fimages%2F2012%2F06%2F20120605_8.jpg",
-        "https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1511015180167&di=3bcedd33a30129b9951be2a81f9b505c&imgtype=0&src=http%3A%2F%2Fpic1.5442.com%2F2015%2F0420%2F06%2F05.jpg"
+        url,
+        url,
+        url
       ],
       mainmenu: [],
       code: "",
