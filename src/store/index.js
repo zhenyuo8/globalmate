9b8bc73a128a3b2a0df785f3801c88acc38533ae
zhoujier <zhoujier@yonyou.com>
2018-09-12 15:51:19 +0800

    公众号得更换

--- src/store/index.js
+++ src/store/index.js
@@ -8,8 +8,10 @@ import config from '../config/config'
 export default new Vuex.Store({
   state: {
     ip: config[__ENV__].apiHost,
-    wxAppId: 'wx4107d508cc1d5171',
-    wxAppSecret: '2d437dabf5d45f5bbaed8f727631c6cb',
+    // wxAppId: 'wx4107d508cc1d5171',
+    wxAppId: 'wxfd37dadae5230dc9',
+    // wxAppSecret: '2d437dabf5d45f5bbaed8f727631c6cb',
+    wxAppSecret: '2e3bb44a9bea31b7ef4adee7fc75045b',
     isWXVerified: false
   },
   mutations: {
