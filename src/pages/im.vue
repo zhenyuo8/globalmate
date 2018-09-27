f750a34aa211f2d3727191af0295cdde1aee262e
huangzhyf <huangzhyf@yonyou.com>
2018-09-27 15:13:46 +0800

    scrollHeight offsetHeight

--- src/pages/im.vue
+++ src/pages/im.vue
@@ -468,6 +468,7 @@ export default {
     },
     scrollTop(){
         setTimeout(()=>{
+            alert(document.body.scrollHeight)
             document.body.scrollBottom = document.querySelector('#main').offsetHeight;
             // document.body.scrollBottom = document.body.scrollHeight;
         },300)
